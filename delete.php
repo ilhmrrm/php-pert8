ea2066b849a1e013d98e119a38219cf24a6fd208
ilhmrrm <ilham26ramadhan@gmail.com>
2024-01-01 19:47:52 +0700

    fixing several bug

--- delete.php
+++ delete.php
@@ -2,17 +2,20 @@
     include('database/config.php');
 
     if (isset($_GET['id'])) {
-
-        $query = mysqli_query($conn, "SELECT * FROM mahasiswa_npm WHERE id = '$id_mahasiswa'");
-        $row = mysqli_fetch_assoc($query);
+        $id_mahasiswa = $_GET['id'];
 
         // Persiapan hapus data
-        $hapus = mysqli_query($conn, "DELETE FROM mahasiswa_npm WHERE id = '$_GET[id]' ");
+        $hapus = mysqli_query($conn, "DELETE FROM mahasiswa_npm WHERE id_mahasiswa = '$_GET[id]' ");
         if ($hapus) {
             echo "<script>
                     alert('Hapus Data Sukses!');
                     document.location='index.php';
                  </script>";
+        } else {
+            echo "<script>
+            alert('Gagal menghapus data!');
+            window.location.href='index.php';
+            </script>";
         }
     }
 ?>
